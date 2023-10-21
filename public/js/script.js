e42c42eb0831529038ea17955db85bdc2a88a2d5
farruhzoirov <fzoirov292gmail.com>
2023-10-21 11:51:44 +0500

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,55 +1,93 @@
 'use strict';
 
-const registerButton = document.querySelector('.webinar-main-button');
-const modalBackdrop = document.querySelector('.modal-backdrop');
-const modalCloserElements = document.querySelectorAll('[data-modal-close]');
-const form = document.querySelector('.form');
 
-registerButton.addEventListener('click', () => {
-    modalBackdrop.classList.add('modal-backdrop--open')
-})
+// Index page
+try {
+    const registerButton = document.querySelector('.webinar-main-button');
+    const modalBackdrop = document.querySelector('.modal-backdrop');
+    const modalCloserElements = document.querySelectorAll('[data-modal-close]');
+    const form = document.querySelector('.form');
 
-function closeModal() {
-    modalBackdrop.classList.remove('modal-backdrop--open');
-}
+    registerButton.addEventListener('click', () => {
+        modalBackdrop.classList.add('modal-backdrop--open')
+    })
 
-window.addEventListener('keydown', e => {
-    if (e.key === 'Escape') {
-        closeModal();
+    function closeModal() {
+        modalBackdrop.classList.remove('modal-backdrop--open');
     }
-})
 
-modalCloserElements.forEach(el => {
-    el.addEventListener('click', e => {
-        if (e.target.hasAttribute('data-modal-close')) {
+    window.addEventListener('keydown', e => {
+        if (e.key === 'Escape') {
             closeModal();
         }
     })
-})
 
-form.addEventListener('submit', async e => {
-    e.preventDefault();
+    modalCloserElements.forEach(el => {
+        el.addEventListener('click', e => {
+            if (e.target.hasAttribute('data-modal-close')) {
+                closeModal();
+            }
+        })
+    })
 
-    const submitButton = e.target.querySelector('.form-button');
-    const name = e.target.querySelector('#name').value.trim();
-    const phone = e.target.querySelector('#phone').value?.replace(/[^0-9]/g, '');
+    form.addEventListener('submit', async e => {
+        e.preventDefault();
 
-    if (name.length && phone?.length === 12) {
-        submitButton.setAttribute('disabled', true);
-        submitButton.textContent = 'Yuborilmoqda...'
+        const submitButton = e.target.querySelector('.form-button');
+        const name = e.target.querySelector('#name').value.trim();
+        const phone = e.target.querySelector('#phone').value?.replace(/[^0-9]/g, '');
 
-        const formData = new FormData();
-        formData.append('Ismi', name);
-        formData.append('Telefon raqami', phone);
-        formData.append(`Ro'yxatdan o'tgan vaqti`, new Date().toLocaleString());
+        if (name.length && phone?.length === 12) {
+            submitButton.setAttribute('disabled', true);
+            submitButton.textContent = 'Yuborilmoqda...'
 
-        let response = await fetch('https://script.google.com/macros/s/AKfycbyirLuhsgD4t4Uc6tk0F995Rdta7XAmP3uM4kwrM3p-jc7Vo4oZOLjHT8p4JE9bMAxQ/exec', {
-            method: 'POST',
-            body: formData
+            let response = await fetch('https://webinar-backend-750b6-default-rtdb.firebaseio.com/users.json', {
+                method: 'POST',
+                body: JSON.stringify({
+                    name, phone, time: new Date().toLocaleString()
+                })
+            })
+
+            if (response.ok) {
+                window.location.href = `${ window.location.protocol }//${ window.location.host }/telegram.html`
+            }
+        }
+    })
+} catch (e) {}
+
+
+// Users page
+
+try {
+    const tbody = document.querySelector('tbody');
+    const refreshButton = document.querySelector('#refresh');
+    const usersCount = document.querySelector('#refresh strong');
+
+    async function getUsersList() {
+        let response = await fetch('https://webinar-backend-750b6-default-rtdb.firebaseio.com/users.json', {
+            method: 'GET'
         })
 
-        if (response.ok) {
-            window.location.href = `${ window.location.protocol }//${ window.location.host }/telegram.html`
+        let users = await response.json();
+        let count = 0;
+        tbody.innerHTML = '';
+        for (const key in users) {
+            count++;
+            tbody.innerHTML += `
+                <tr>
+                    <td>${ count }</td>
+                    <td>${ users[key].name }</td>
+                    <td>${ users[key].phone }</td>
+                    <td>${ users[key].time }</td>
+                </tr>
+            `
         }
+        usersCount.textContent = count;
     }
-})
+
+    getUsersList();
+
+    refreshButton.addEventListener('click', getUsersList);
+} catch (e) {
+    
+}

db8c76fb435f8182d0d856f83fc28ede1e986928
sultonbayevogabek <sultonbayevogabek@gmail.com>
2023-10-23 12:21:29 +0500

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -72,30 +72,56 @@ try {
     const tbody = document.querySelector('tbody');
     const refreshButton = document.querySelector('#refresh');
     const usersCount = document.querySelector('#refresh strong');
+    const pagination = document.querySelector('#pagination');
+    let page = 1;
 
     async function getUsersList() {
         let response = await fetch('https://webinar-backend-750b6-default-rtdb.firebaseio.com/users.json', {
             method: 'GET'
         })
 
-        let users = await response.json();
+        const usersObjects = await response.json()
+        const users = [];
         let count = 0;
-        tbody.innerHTML = '';
-        for (const key in users) {
+        for (const key in usersObjects) {
             count++;
-            tbody.innerHTML += `
-                <tr>
-                    <td>${ count }</td>
-                    <td>${ users[key].name }</td>
-                    <td style="display: flex; justify-content: space-between">
-                        <span>${ users[key].phone }</span>
-                        <button style="cursor: pointer; margin-left: auto" data-clipboard>Copy</button>
-                    </td>
-                    <td>${ users[key].time }</td>
-                </tr>
-            `
+            users.push({
+                id: count,
+                ...usersObjects[key]
+            })
         }
         usersCount.textContent = count;
+
+        pagination.innerHTML = '';
+        for (let i = 0; i < Math.ceil(count / 100); i++) {
+            pagination.innerHTML += `
+                <button data-paginator-button style="margin: 10px 2px; cursor: pointer">${ i + 1 }</button>
+            `
+        }
+
+        document.querySelectorAll('[data-paginator-button]')?.forEach(button => {
+            button.addEventListener('click', e => {
+                page = +e.target.textContent;
+                getUsersList();
+            })
+        })
+
+        tbody.innerHTML = '';
+        users.slice((page - 1) * 100, page * 100)?.forEach(user => {
+            tbody.innerHTML += `
+                <tr>
+                    <td>${ user.id }</td>
+                    <td>${ user.name }</td>
+                    <td>
+                        <div style="display: flex; justify-content: space-between">
+                            <span>${ user.phone }</span>
+                            <button style="cursor: pointer; margin-left: auto" data-clipboard>Copy</button>
+                        </div>
+                    </td>
+                    <td>${ user.time }</td>
+                </tr>
+            `
+        })
 
         document.querySelectorAll('[data-clipboard]').forEach(i => {
             i.addEventListener('click', e => {
